04a71fa041775170b725e86920c5f23498c2060b
dingqiyu <1195261106@qq.com>
2019-10-25 22:31:21 +0800

    模板

---
+++ src/main/java/com/qfedu/ordersystem/OrdersystemApplication.java
@@ -0,0 +1,17 @@
+package com.qfedu.ordersystem;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@MapperScan("com.qfedu.com.qfedu.ordersystem.dao")
+@EnableSwagger2
+public class OrdersystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrdersystemApplication.class, args);
+    }
+
+}
--- src/main/java/com/qfedu/ordersystem/dao/MenuDao.java
+++
@@ -1,7 +0,0 @@
-package com.qfedu.ordersystem.dao;
-
-import com.qfedu.ordersystem.entry.Menu;
-
-public interface MenuDao {
-    Menu selectById();
-}
