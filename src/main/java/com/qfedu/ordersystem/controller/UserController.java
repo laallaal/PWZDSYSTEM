b7a44cfcc9642e2c4a959f8ce4eb02247492e103
150536371@qq.com <1505363731@qq.com>
2019-10-26 10:49:36 +0800

    测试

---
+++ src/main/java/com/qfedu/ordersystem/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.qfedu.ordersystem.controller;
+
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/user")
+@Api(value = "user", tags = "这是用户可以操作的功能")
+public class UserController {
+
+
+    @PostMapping("/all")
+    @ApiOperation(value = "aaa", notes = "sss", tags = "dddd")
+    public void all() {
+
+
+    }
+
+}
